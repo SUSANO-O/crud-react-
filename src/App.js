1784a078d862423dbe687d9f3da4001901df133b
harold <harvar@MacBook-Pro-de-harvar.local>
2019-12-30 19:04:14 -0500

    CRUD react use hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import UseTable from './tables/UseTable'
+import AddUserForm from './forms/AddUserForm'
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'tania', username: 'diaz'},
+    { id: 2, name: 'andres', username: 'vargas'},
+    { id: 3, name: 'maria', username: 'perez'}
+  ]
+
+  const [ users, setUsers] = useState(usersData)
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
+  const deleteUser = (id) => {
+    setUsers(users.filter(user => user.id !==id))
+  }
+	
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+          <AddUserForm  addUser={addUser}   />
+
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        <UseTable  users={users} deleteUser={deleteUser}  />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+// import React, { useState, Fragment } from 'react'
+// import AddUserForm from './forms/AddUserForm'
+// import UserTable from './tables/UseTable'
+
+// const App = () => {
+// 	const usersData = [
+// 		{ name: 'Tania', username: 'floppydiskette' },
+// 		{ name: 'Craig', username: 'siliconeidolon' },
+// 		{ name: 'Ben', username: 'benisphere' },
+// 	]
+
+// 	const initialFormState = { name: '', username: '' }
+
+// 	// Setting state
+// 	const [ users, setUsers ] = useState(usersData)
+// 	const [ currentUser, setCurrentUser ] = useState(initialFormState)
+
+// 	// CRUD operations
+// 	const addUser = user => {
+// 		setUsers([ ...users, user ])
+// 	}
+
+	
+
+// 	const deleteUser = id => {
+// 		setUsers(users.filter(user => user.name !== id))
+// 	}
+
+
+
+// 	return (
+// 		<div className="container">
+// 			<h1>CRUD App with Hooks</h1>
+// 			<div className="flex-row">
+// 				<div className="flex-large">
+// 							<h2>Add user</h2>
+// 							<AddUserForm addUser={addUser} />
+// 					)
+// 				</div>
+// 				<div className="flex-large">
+// 					<h2>View users</h2>
+
+// 					<UserTable users={users} deleteUser={deleteUser} />
+// 				</div>
+// 			</div>
+// 		</div>
+// 	)
+// }
+
+// export default App
